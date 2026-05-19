a814a7486acce54cb0e01d5572486ebd860edb8b
Raffa Freitas <86832858+Raffafe@users.noreply.github.com>
2026-05-19 10:31:37 +0100

    Update Index url meses

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -41,6 +41,26 @@ const Index = ({ initialTab = "loja" }: IndexProps) => {
   if (mappedCategory && CATEGORIES.includes(mappedCategory)) {
     setCategory(mappedCategory);
   }
+      const monthMap: Record<string, number> = {
+  janeiro: 1,
+  fevereiro: 2,
+  marco: 3,
+  abril: 4,
+  maio: 5,
+  junho: 6,
+  julho: 7,
+  agosto: 8,
+  setembro: 9,
+  outubro: 10,
+  novembro: 11,
+  dezembro: 12,
+};
+
+const mappedMonth = monthMap[categoria];
+
+if (mappedMonth) {
+  setMonth(mappedMonth);
+}
 }, [categoria]);
 
   const now = new Date();
